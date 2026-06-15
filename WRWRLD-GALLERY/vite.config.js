c00accb4972f3670d7153b30da07c33ca1c6ff4e
Wrewww <106407550+Wrewww@users.noreply.github.com>
2026-06-15 13:38:05 +0400

    Update vite.config.js

--- WRWRLD-GALLERY/vite.config.js
+++ WRWRLD-GALLERY/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/WRWRLDHUB/WRWRLD-GALLERY/',
-  build: {
-    outDir: '../dist/WRWRLD-GALLERY',
-    emptyOutDir: true,
-  },
-});
+})
