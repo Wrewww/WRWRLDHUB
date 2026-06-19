7fe29b7df0eca6d3572f3ce249fa67bea5d06504
Wrewww <Xmorusik@gmail.com>
2026-06-20 02:35:42 +0400

    main

--- WRWRLD-GALLERY/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import ThemeToggle from './ThemeToggle';
-
-export default function Header({ isLightTheme, onToggleTheme }) {
-  return (
-    <header className="site-header">
-      <ThemeToggle isLightTheme={isLightTheme} onToggleTheme={onToggleTheme} />
-    </header>
-  );
-}
